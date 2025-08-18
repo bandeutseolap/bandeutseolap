9561901c872654dae62e90c075d364a91cb7ac7c
yelin <ggg06141@gmail.com>
2025-08-18 21:38:50 +0900

    add frontend

---
+++ backend/src/main/java/com/dobidan/bandeutseolap/BandeutseolapApplication.java
@@ -0,0 +1,13 @@
+package com.dobidan.bandeutseolap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandeutseolapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandeutseolapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dobidan/bandeutseolap/BandeutseolapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobidan.bandeutseolap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandeutseolapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
