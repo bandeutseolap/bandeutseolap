21c3e9fe317f07e5a3d85eab5db056fda73134eb
yelin <ggg06141@gmail.com>
2025-08-31 21:22:48 +0900

    testcontroller로 패키지명 변경

--- backend/src/main/java/com/dobidan/bandeutseolap/testcontroller/HelloWorldController.java
+++ backend/src/main/java/com/dobidan/bandeutseolap/testcontroller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.dobidan.bandeutseolap.Controller;
+package com.dobidan.bandeutseolap.testcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
-//import logo from './logo.svg';
-//import './App.css';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function App() {
-  const [hello, setHello] = useState('');
+    const [hello, setHello] = useState('');
 
-  useEffect(() => {
-          axios.get('/api/hello')
-          .then(response => setHello(response.data))
-          .catch(error => console.log(error))
-      }, []);
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
 
-  return (
-    <div className="App">
-      백엔드에서 가져온 데이터 확인 : {hello}
-    </div>
-  );
+    return (
+        <div className="App">
+            백엔드에서 가져온 데이터 확인 : {hello}
+        </div>
+    );
 }
 
 export default App;
