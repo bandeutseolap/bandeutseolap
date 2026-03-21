e7406670308c8065c7119bb321b1ffc0979e64b7
nikeisme <nikeisme827@gmail.com>
2026-03-21 16:58:28 +0900

    feat: Kafka 로그인 이벤트 Producer/Consumer 구현 및 로그인 이력 저장

---
+++ backend/src/main/java/com/dobidan/bandeutseolap/global/kafka/LoginEventConsumer.java
@@ -0,0 +1,58 @@
+package com.dobidan.bandeutseolap.global.kafka;
+
+import com.dobidan.bandeutseolap.domain.user.entity.UserLog;
+import com.dobidan.bandeutseolap.domain.user.repository.UserLogRepository;
+import com.dobidan.bandeutseolap.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LoginEventConsumer {
+
+    private final UserLogRepository userLogRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * consumeLoginEvent()
+     *
+     * - "login-event" 토픽에서 메시지 수신
+     * - 메시지 형식 : "username,ipAddress,action"
+     * - 파싱 후 UserLog DB 저장
+     */
+
+    @KafkaListener(topics = "auth.login.event",groupId= "bandeutseolap")
+    public void consumeLoginEvent (String message){
+        log.info("kaka 이벤트 수신 - message : {}",message);
+
+        //1.메시지파싱
+        String[] parts = message.split(",");
+        String username = parts[0];
+        String ipAddress = parts[1];
+        String action = parts[2];
+
+        //2.username으로 userId 조회
+        Long userId = userRepository.findByUsername(username)
+                .map(user -> user.getId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+
+        // 3. UserLog 저장
+        UserLog userLog = UserLog.builder()
+                .userId(userId)
+                .action(action)
+                .ipAddress(ipAddress)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        userLogRepository.save(userLog);
+        log.info("로그인 이력 저장 완료 - username: {}, action: {}", username, action);
+
+    }
+
+
+}
